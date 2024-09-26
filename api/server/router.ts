438fd427d95bfa36ac50ff53882aa2e0e0f20fc3
Joséphine Robin <josephine@MacBook-Pro-de-Josephine.local>
2024-09-26 16:45:35 +0200

    Adds validation for POST

--- api/server/router.ts
+++ api/server/router.ts
@@ -1,31 +1,38 @@
-import { Router } from 'express';
+import { Router, NextFunction } from 'express';
 import reposDatas from '../data/formated_repos.json'
-// Fichier des routes
+
+// Outil pour la validation de données
+import Joi from 'joi';
 
 const router = Router();
 
-const addLanguage = async (req: any, res: any, next: any) => {
-    try {
-        const body = req.body;
-        console.log(body);
-        res.sendStatus(200);
-    } catch (error) {
-        next(error);
-    }
+// Schema de validation
+const schema = Joi.object({
+    id: Joi.string().required(),
+    name: Joi.string().required(),
+    url: Joi.string().required(),
+    languages: Joi.array().required()
+});
+
+// On valide les données avant de les envoyer en BDD à l'aide de JOI
+const validateRepoData = (req:any, res:any, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+
+    if (!error) next();
+
+    // 422 Unprocessable Entity
+    res.status(422).json(error);
 }
 
-router.post('/languages', addLanguage);
-
+// GET tous les repos
 router.get('/', (_, res)=>{
     res.status(200).json(reposDatas);
 });
 
-router.post('/', (req, res)=>{
-    console.log("Dans le post de repo, req", req);
+// POST un nouveau repo et renvoie la liste modifiee
+router.post('/', validateRepoData, (req, res)=>{
     reposDatas.push(req.body);
     res.status(201).json(reposDatas);
 });
-
-
 
 export default router;
